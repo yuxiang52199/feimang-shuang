538d2f60d6916282ddad0a2fc792629b5c8e6f16
xiaosa2018 <276307378@qq.com>
2018-03-27 10:48:55 +0800

    修改注释

---
+++ feimang-server/feimang-server-month/src/main/java/com/feimang/month/vo/UserRDBVo.java
@@ -0,0 +1,45 @@
+package com.feimang.month.vo;
+
+import com.feimang.month.pojo.UserAbstruct;
+
+public class UserRDBVo {
+    private int userRDBId;
+    private Long readingDates;
+    private  int readingBooks;
+    private UserAbstruct userAbstruct;
+
+    public UserAbstruct getUserAbstruct() {
+        return userAbstruct;
+    }
+
+    public void setUserAbstruct(UserAbstruct userAbstruct) {
+        this.userAbstruct = userAbstruct;
+    }
+
+    public Long getReadingDates() {
+        return readingDates;
+    }
+
+    public void setReadingDates(Long readingDates) {
+        this.readingDates = readingDates;
+    }
+
+
+
+    public int getUserRDBId() {
+        return userRDBId;
+    }
+
+    public void setUserRDBId(int userRDBId) {
+        this.userRDBId = userRDBId;
+    }
+
+
+    public int getReadingBooks() {
+        return readingBooks;
+    }
+
+    public void setReadingBooks(int readingBooks) {
+        this.readingBooks = readingBooks;
+    }
+}
